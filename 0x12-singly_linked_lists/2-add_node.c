c59df2e0f5efc6b1903c1706f1c6c0b37224320e
DevMustrowk <elmustapha.abourar@outlook.com>
2022-03-18 11:46:48 +0100

    Return Number of Element

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/*add_node - add a new node at the beginning of a list.
+ *@head: first node in the list.
+ *@str: second node in the list
+ *return: the adress of the new element, or NULL if given a failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	}
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LIST_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -21,5 +22,7 @@ typedef struct list_s
 /*prototypes*/
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
 
 #endif
