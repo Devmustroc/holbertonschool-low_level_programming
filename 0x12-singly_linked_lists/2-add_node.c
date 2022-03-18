5d1cc2331da4b0a6ace885a4e3fa13fd05e5a112
DevMustrowk <elmustapha.abourar@outlook.com>
2022-03-18 11:48:35 +0100

    Return Number of Element

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-/*add_node - add a new node at the beginning of a list.
+/*
+ *add_node - add a new node at the beginning of a list.
  *@head: first node in the list.
  *@str: second node in the list
  *return: the adress of the new element, or NULL if given a failed.
