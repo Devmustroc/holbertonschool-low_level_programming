d8b0c4adc26376c8c563f7f4d1697041f9628f65
DevMustrowk <elmustapha.abourar@outlook.com>
2022-03-22 20:45:33 +0100

    Error Return Fixed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,6 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			removeNode = temp->next;
 			temp->next = removeNode->next;
 			free(removeNode);
+			return (1);
 		}
 		temp = temp->next;
 		i++;
