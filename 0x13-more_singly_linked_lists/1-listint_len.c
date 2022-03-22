aa8f901f7c1a05d2f17f22a313c875528f2d2650
DevMustrowk <elmustapha.abourar@outlook.com>
2022-03-22 10:57:29 +0100

    Betty requierement

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 /**
  * listint_len - returns the number of elements in a linked listint_t list.
  * @h: head pointer.
- * return: number of elements
+ * Return: number of elements
  */
 size_t listint_len(const listint_t *h)
 {
