6dfb981af3a4970e443842dbe2bd3c6627df2a51
DevMustrowk <elmustapha.abourar@outlook.com>
2022-04-03 20:09:51 +0200

    fix main

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,52 @@
 #include "main.h"
 
 /**
- * times_table - prints the times table up to 9
+ * print_times_table - prints the times table up to 9
  * @n: integer.
  *
  * Return: On success 1, -1 if an error.
  */
+
 void print_times_table(int n)
 {
 	int i, j, r;
+
 	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
 		{
-			r = i * j;
-			if (j != 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar (',');
-				_putchar (' ');
-				if (r >= 10 && r < 100)
+				r = i * j;
+				if (j != 0)
 				{
+					_putchar (',');
 					_putchar (' ');
-					_putchar (r / 10 + '0');
-					_putchar (r % 10 + '0');
-				}
-				else if (r < 10)
-				{
-					_putchar (' ');
-					_putchar (' ');
-					_putchar (r + '0');
+					if (r >= 10 && r < 100)
+					{
+						_putchar (' ');
+						_putchar (r / 10 + '0');
+						_putchar (r % 10 + '0');
+					}
+					else if (r < 10)
+					{
+						_putchar (' ');
+						_putchar (' ');
+						_putchar (r + '0');
+					}
+					else
+					{
+						_putchar (r / 100 + '0');
+						_putchar ((r / 10) % 10 + '0');
+						_putchar (r % 10 + '0');
+					}
 				}
 				else
 				{
-					_putchar (r / 100 + '0');
-					_putchar ((r / 10) % 10 + '0');
-					_putchar (r % 10 + '0');
+					_putchar (r  + '0');
 				}
 			}
-			else
-			{
-				_putchar (r  + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-}
-}
+	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,3 +17,4 @@ void more_numbers(void);
 void print_times_table(int n);
 void print_times_table(int n);
 
+#endif
