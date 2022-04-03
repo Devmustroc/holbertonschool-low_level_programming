9caebd629fe3b24ff9a19b3df129bc3bce67bcbf
DevMustrowk <elmustapha.abourar@outlook.com>
2022-04-03 20:19:07 +0200

    get point

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,5 +16,8 @@ void print_to_98(int n);
 void more_numbers(void);
 void print_times_table(int n);
 void print_times_table(int n);
+void fibonacci_93(unsigned long a, unsigned long b);
+int main(void);
+
 
 #endif
