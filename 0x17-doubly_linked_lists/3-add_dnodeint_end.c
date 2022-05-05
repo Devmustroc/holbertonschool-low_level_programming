96200ee585685b60c7928742d9269a6f830054f2
devmustroc <elmustapha.abourar@outlook.com>
2022-05-05 12:40:24 +0200

    linked list update

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - add node at the end of a double linked list
+ * @head: pointer to head of a Double Linked list
+ * @n: data to store in a Linked list
+ * @return adresse of new pointer.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *b, *temp;
+
+	b = malloc(sizeof(dlistint_t));
+	b->next = b->prev = NULL;
+	b->n = n;
+
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = b;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		b->prev = temp;
+		temp->next = b;
+	}
+	return (b);
+
+}
