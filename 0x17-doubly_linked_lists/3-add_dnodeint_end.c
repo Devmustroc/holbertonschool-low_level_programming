d511bd495e6dfe99ef08e677e00b6859bdcf3c2a
devmustroc <elmustapha.abourar@outlook.com>
2022-05-05 12:59:39 +0200

    fix betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,33 +3,37 @@
  *add_dnodeint_end- add node at the end of a double linked list
  *@head: pointer to head of a Double Linked list
  *@n: data to store in a Linked list
- *@return adresse of new pointer.
+ *Return: adresse of new pointer.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *b, *temp;
 
-	b = malloc(sizeof(dlistint_t));
-	b->next = b->prev = NULL;
-	b->n = n;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 
+	b = malloc(sizeof(dlistint_t));
 	if (b == NULL)
 	{
 		return (NULL);
 	}
+	b->next = NULL;
+	b->n = n;
 	if (*head == NULL)
 	{
+		b->prev = NULL;
 		*head = b;
+		return (b);
 	}
-	else
-	{
-		temp = *head;
+
+	temp = *head;
 		while (temp->next != NULL)
 		{
 			temp = temp->next;
 		}
 		b->prev = temp;
 		temp->next = b;
-	}
 	return (b);
 }
