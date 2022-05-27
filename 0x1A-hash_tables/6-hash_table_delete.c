3330f5d606ec48ffb8f5b1fd0c93d1ca6cd97e87
devmustroc <elmustapha.abourar@outlook.com>
2022-05-27 17:10:14 +0200

    hashing tables

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete table
+ * @ht: pointer to struct of hash table
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned int i = 0;
+    hash_node_t *ptr = NULL;
+    hash_node_t *head = NULL;
+
+    while (i < ht->size)
+    {
+        ptr = ht->array[i];
+        while (ptr)
+        {
+            head = ptr->next;
+            free(ptr->value);
+            free(ptr->key);
+            free(ptr);
+            ptr = head;
+        }
+        i++;
+    }
+    free(ht->array);
+    free(ht);
+}
