a983a69cd757638b69daeb871ddbdcffaf5087e5
devmustroc <elmustapha.abourar@outlook.com>
2022-05-27 17:21:32 +0200

    betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,39 +6,39 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *ptr = NULL;
-    unsigned int index = 0, temp = 0;
+	hash_node_t *ptr = NULL;
+	unsigned int index = 0, temp = 0;
 
-    if(ht == NULL)
-    {
-        return;
-    }
-    printf("{");
+	if(ht == NULL)
+	{
+		return;
+	}
+	printf("{");
 
-    while(index < ht->size)
-    {
-        if(ht->array[index] == NULL)
-        {
-            index++;
-            continue;
-        }
-        if (temp == 1)
-        {
-            printf(", ");
-        }
-        temp = 1;
-        ptr = ht->array[index];
+	while(index < ht->size)
+	{
+		if(ht->array[index] == NULL)
+		{
+			index++;
+			continue;
+		}
+		if (temp == 1)
+		{
+			printf(", ");
+		}
+		temp = 1;
+		ptr = ht->array[index];
 
-        while(ptr)
-        {
-            printf("'%s' : '%s'", ptr->key, ptr->value);
-            if (ptr->next != NULL)
-            {
-                printf(", ");
-            }
-            ptr = ptr->next;
-        }
-        index++;
-    }
-    printf("}\n");
+		while(ptr)
+		{
+			printf("'%s' : '%s'", ptr->key, ptr->value);
+			if (ptr->next != NUL)
+			{
+				printf(", ");
+			}
+			ptr = ptr->next;
+		}
+		index++;
+	}
+	printf("}\n");
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,23 +6,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned int i = 0;
-    hash_node_t *ptr = NULL;
-    hash_node_t *head = NULL;
+	unsigned int i = 0;
+	hash_node_t *ptr = NULL;
+	hash_node_t *head = NULL;
 
-    while (i < ht->size)
-    {
-        ptr = ht->array[i];
-        while (ptr)
-        {
-            head = ptr->next;
-            free(ptr->value);
-            free(ptr->key);
-            free(ptr);
-            ptr = head;
-        }
-        i++;
-    }
-    free(ht->array);
-    free(ht);
+	while (i < ht->size)
+	{
+		ptr = ht->array[i];
+
+		while (ptr)
+		{
+			head = ptr->next;
+			free(ptr->value);
+			free(ptr->key);
+			free(ptr);
+			ptr = head;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
 }
